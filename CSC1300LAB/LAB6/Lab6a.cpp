fd303feb7f8e8db364d5fc50d95857c81b369e14
KamdenE <kamdenedens@gmail.com>
2022-03-05 09:46:40 -0600

    Lab6 Update

---
+++ CSC1300LAB/LAB6/Lab6a.cpp
@@ -0,0 +1,8 @@
+/*
+	Title:  	Lab6a.cpp
+	Author:		Kamden Edens
+	Date:		3/4/2022
+	Purpose:	A program that uses for loops
+                to build a pyramid according to
+                what number the user inputs
+*/
