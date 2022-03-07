3708705b822821557e3391ef7e2538bc7c28e062
KamdenE <kamdenedens@gmail.com>
2022-03-07 13:12:39 -0600

    LAB7

---
+++ CSC1300LAB/LAB7/Lab7a.cpp
@@ -0,0 +1,10 @@
+/*
+	Title:  	Lab7a.cpp
+	Author:		Kamden Edens
+	Date:		3/7/2022
+	Purpose:	A program that caclulates sick days
+                for the bank.
+*/
+
+#include <iostream>
+using namespace std;
