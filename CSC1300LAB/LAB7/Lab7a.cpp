c2cf17a28d478c56c584b0c58184714fabf2cf2c
KamdenE <kamdenedens@gmail.com>
2022-03-08 21:11:19 -0600

    Lab7

--- CSC1300LAB/LAB7/Lab7a.cpp
+++ CSC1300LAB/LAB7/Lab7a.cpp
@@ -8,3 +8,40 @@
 
 #include <iostream>
 using namespace std;
+
+// Function
+bool displayMenuGetChoice() {
+        int firstAnswer;
+        cout << "\nPlease choose from the following options:" << endl;
+        cout << "\n1. Enter Employee Days" << endl;
+        cout << "2. End program" << endl;
+        cin >> firstAnswer;
+        // checks the user's input
+        while (cin.fail() || firstAnswer != 1 && firstAnswer != 2) {
+            cin.clear();
+            cin.ignore(255, '\n');
+            cout << "Invalid input. Please try again. " << endl;
+            cin >> firstAnswer;
+        }
+        switch (firstAnswer) {
+            case 1:
+                return true;
+                break;
+            case 2:
+                return false;
+                break;
+            default:
+                cout << "Error occurred." << endl;
+                return false;
+                break;
+        }
+}
+// Main function
+
+int main() {
+    //variables
+    int numEmployees, sickDays, total;
+
+}
+
+
