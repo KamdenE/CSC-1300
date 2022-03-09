8e0c387fe7093d13c048031d0697bfedff590fbd
KamdenE <kamdenedens@gmail.com>
2022-03-09 10:40:36 -0600

    Array test

---
+++ In_Class_Programs/arrayTest1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int numbers[100];
+
+    for(int x=0; x<100; x++) {
+        numbers[x] = 0;
+        cout << numbers[x] << endl;
+    }
+    
+    return 0;
+}
