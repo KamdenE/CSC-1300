027374afadaeca0352d2fbbe01751f88602ec87c
Kamden Edens <edens.kamden@gmail.com>
2022-03-24 18:16:34 -0500

    Lab8 update

--- CSC1300LAB/LAB8/driver.cpp
+++ CSC1300LAB/LAB8/driver.cpp
@@ -1,4 +1,18 @@
-#ifndef ELEPHANT_H
-#define ELEPHANT_H
-//all your code for your header file
-#endif
+/*
+	Title:  	Lab8a.cpp
+	Author:		Kamden Edens
+	Date:		3/24/2022
+	Purpose:	A program that uses a header file
+                and functions.
+*/
+
+#include "elephant.h"
+
+int main()
+{
+   int elephantAmt[10]; 
+
+
+
+    return 0;
+}
---
+++ CSC1300LAB/LAB8/functions.cpp
@@ -0,0 +1,10 @@
+/*
+	Title:  	Lab8a.cpp
+	Author:		Kamden Edens
+	Date:		3/24/2022
+	Purpose:	A program that uses a header file
+                and functions.
+*/
+
+#include "elephant.h"
+
