6b428bae51ebc4b9aa24e3f89e7e850e7b444b6c
KamdenE <kamdenedens@gmail.com>
2022-03-25 15:43:35 -0500

    Lab 8 complete

--- CSC1300LAB/LAB8/driver.cpp
+++ CSC1300LAB/LAB8/driver.cpp
@@ -13,26 +13,35 @@
 #include <iomanip>
 using namespace std;
 
-
+// variables 
 const int SIZE = 10;
 int elephantAmt[SIZE]; 
 char zooName[100];
+string line(50, '-');
 
 int main()
 {
+    //local variables to main
     float foodTotal, foodAverage;
     int elePig;
     float numPounds[SIZE];
     string elephantName[SIZE];
-
+    // gets the zoo name
     cout << "Greetings! What is the name of your zoo? "; 
     cin.getline(zooName, 100);
 
+    // executes the functions
     GETELEPHANTDATA(numPounds, elephantName);
 
     GETSTATS(numPounds, foodTotal, foodAverage, elePig);
 
+    // output 
 
+    cout << line << endl;
+    cout << "The total amount of food consumed by all 10 elephants at " << zooName << " is " << foodTotal << " pounds in one month!" << endl;
+    cout << "\nThe average amount of food consumed at " << zooName << " is " << foodAverage << "." << endl;
+    cout << "\n\n\nThe elephant that ate the most (" << numPounds[elePig] << " pounds) is " << elephantName[elePig] << "!!" << endl;
+    cout << line << endl;
 
 
     return 0;
--- CSC1300LAB/LAB8/functions.cpp
+++ CSC1300LAB/LAB8/functions.cpp
@@ -11,21 +11,21 @@
 #include <string>
 using namespace std;
 
-
+// Function 1 that returns foodAmt and Elephant name to the arrays
 void GETELEPHANTDATA(float foodAmt[], string elephantName[]) {
     cout << "Enter in the name and how many pounds of food each elephant consumed last month: " << endl;
     for (int x = 0; x < 10; x++)
 	{
 		cout << "\nElephant #" << (x + 1) << ": ";
-        cout << "     Name: "; 
+        cout << "\n     Name: "; 
         getline(cin, elephantName[x]);
-        cin.ignore();
-        cout << "\n     Food Amount: "; 
+        cout << "     Food Amount: "; 
         cin >> foodAmt[x];
+        cin.ignore();
 	}
     
 }
-
+// Function 2 that returns several variables to the main function
 void GETSTATS(float foodAmt[], float& foodTotal, float& foodAverage, int& elePig) {
     for (int x = 0; x < 10; x++) {
         foodTotal += foodAmt[x];
