411d831b60b224e42ed9bf45590fb0cacf55df3f
KamdenE <kamdenedens@gmail.com>
2022-04-05 11:58:44 -0500

    prog 3 work

--- Programs/Program 3/kedens43_prog3.cpp
+++ Programs/Program 3/kedens43_prog3.cpp
@@ -20,15 +20,18 @@ bool searchHerbivore(string);
 void printDino(ofstream&, ifstream&, string, int[]);
 bool overTenGrand(string);
 void printResults(int[], string[]);
-string filename;
-int totalsArray[6] = {0};
-string labelsArray[6] = {"TOTAL DINOS", "TOTAL CARNIVORE", "TOTAL HERBIVORE DINOS", "DINOS OVER 10,000", "DINO NAMES END IN 'SAURUS'", "ANIMALS NOT DINOS"};
+
 
 int main() {
-    ifstream inputfile;
-    ofstream dinoOutfile;
-    ofstream carnOutfile;
-    ofstream harbOutfile;
+
+    string filename;
+    int totalsArray[6] = {0};
+    string labelsArray[6] = {"TOTAL DINOS", "TOTAL CARNIVORE", "TOTAL HERBIVORE DINOS", "DINOS OVER 10,000", "DINO NAMES END IN 'SAURUS'", "ANIMALS NOT DINOS"};
+
+    ifstream dinofile;
+    ofstream otherOutFile;
+    ofstream carnOutFile;
+    ofstream harbOutFile;
 
     cout << "Enter in the file name (dinoDirectory.txt): ";
     getline(cin, filename);
@@ -39,12 +42,21 @@ int main() {
         getline(cin, filename);
     }
 
-    inputfile.open(filename);
+    dinofile.open(filename);
 
-    if(inputfile.is_open()) {
+    if(dinofile.is_open()) {
         while(getline(filename, tempString, '#')){
-
+            
+            if(filename.find, 'saurus') {
+                //increment totalsArray 5
+            }
+            // call carnOrHerb
+            //open correct outfile
+            // call print dino
+            //close file end loop
         }
+        //close input file
+        //call printResults sending the arrays
     }
 
 
@@ -53,7 +65,22 @@ int main() {
 
 
 int carnOrHerb(animalName) {
-    cout << "yes";
+    string Carn, Herb, Other;
+
+    searchCarnivore(Carn);
+
+    searchHerbivore(Herb); 
+
+    if(Carn == true) {
+        return 1;
+    }
+
+    else if(Herb == true) {
+        return 2;
+    }
+    else {
+        return -1;
+    }
 }
 bool searchCarnivore(animalSearch) {
     bool isCarn = false;
@@ -66,9 +93,8 @@ bool searchCarnivore(animalSearch) {
         }
     }
     return isCarn;
-
-   
 }
+
 bool searchHerbivore(animalSearch) {
     bool isHerb = false;
     ifstream herbfile;
@@ -82,17 +108,42 @@ bool searchHerbivore(animalSearch) {
     return isHerb;
 }
 void printDino(ofstream& outfile, ifstream& infile, string animalName, int totalsA[]) {
-
-
+    ofstream file;
+    
 }
 bool overTenGrand(weightCheck) {
     string mass;
     bool weightCompare;
+    int num;
+    // int num = stoi(mass) should convert
 
-    if(mess.find, ' lbs') {
-        mass.erase
+    if(mass.find, '30 to 60 lbs') {
+        //mass.erase(' lbs')
+        //int num = stoi(mass)
+        weightCompare = false;
     }
+    else if(mass.find, '5,000 to 9,000 lbs') {
+        weightCompare = false;
+    }
+    else if(mass.find, '9,000 to 10,000 lbs') {
+        weightCompare = true;
+    }
+    else if(mass.find, '11,000 to 15,000 lbs') {
+        weigthCompare = true;
+    }
+    else {
+        cout << "Error";
+    }
+    return weigthCompare;
 }
 void printResults(totalsA[], labelsA[]) {
-
+    totalsA[];
+    labelsA[];
+    int numElements = 7;
+    string line(50, '-');
+    cout << line;
+    for(int x = 0; x < numElements; x++) {
+        cout << labelsA[x] << totalsA[x] << endl;
+    }
+    cout << line;
 }
