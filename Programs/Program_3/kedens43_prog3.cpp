4b2021690607517bcb6817adff458678edde5ed0
KamdenE <kamdenedens@gmail.com>
2022-04-06 15:29:31 -0500

    Program 3 updates

--- Programs/Program_3/kedens43_prog3.cpp
+++ Programs/Program_3/kedens43_prog3.cpp
@@ -30,15 +30,16 @@ int main() {
     string labelsArray[6] = {"TOTAL DINOS", "TOTAL CARNIVORE", "TOTAL HERBIVORE DINOS", "DINOS OVER 10,000", "DINO NAMES END IN 'SAURUS'", "ANIMALS NOT DINOS"};
     size_t find;
     ifstream dinofile;
-    ofstream otherOutFile;
-    ofstream carnOutFile;
-    ofstream herbOutFile;
+    //ofstream otherOutFile;
+    //ofstream carnOutFile;
+    //ofstream herbOutFile;
+    ofstream outStreamFile;
 
     cout << "Enter in the file name (dinoDirectory.txt): ";
     getline(cin, filename);
 
     while(filename != "dinoDirectory.txt") {
-        "Please try to enter the file name in again: "
+        cout << "Please try to enter the file name in again: ";
         cin.ignore();
         getline(cin, filename);
     }
@@ -46,9 +47,9 @@ int main() {
     dinofile.open(filename);
 
     if(dinofile.is_open()) {
-        while(getline(filename, tempString, '#')){
+        while(getline(dinofile, tempString, '#')){
             
-            find = tempstring.find("saurus");
+            find = tempString.find("saurus");
             if(find != string::npos) {
                 totalsArray[4] += 1;
             }
@@ -56,23 +57,26 @@ int main() {
                 case 1:
                     totalsArray[0] += 1;
                     totalsArray[1] += 1;
-                    printDino(carnOutFile, dinofile, tempString, totalsArray[]);
+                    outFileStream.open("carnivore.txt");
+                    printDino(carnOutFile, dinofile, tempString, totalsArray);
                     break;
                 case 2:
                     totalsArray[0] += 1;
                     totalsArray[2] += 1;
-                    printDino(herbOutFile, dinofile, tempString, totalsArray[]);
+
+                    printDino(herbOutFile, dinofile, tempString, totalsArray);
                     break;
                 case -1:
                     totalsArray[5] += 1;
-                    printDino(otherOutFile, dinofile, tempString, totalsArray[]);
+                    printDino(otherOutFile, dinofile, tempString, totalsArray);
                     break;
             }
-            //use tempstring to send overtengrand a number using findvn
-            printDino(tempString)
         }
         dinofile.close();
-        printResults();
+        printResults(totalsArray, labelsArray);
+        otherOutFile.close();
+        carnOutFile.close();
+        herbOutFile.close();
     }
 
 
@@ -83,52 +87,54 @@ int main() {
 int carnOrHerb(string animalName) {
 
     if(searchCarnivore(animalName)) {
+        cout << animalName << " is going in the CARNIVORE file!" << endl;
         return 1;
     }
 
     else if(searchHerbivore(animalName)) {
+        cout << animalName << " is going in the HERBIVORE file!" << endl;
         return 2;
     }
 
     else {
+        cout << animalName << " is going in the OTHER file!" << endl;
         return -1;
     }
 }
 
 bool searchCarnivore(string animalSearch) {
-    bool isCarn = false;
     ifstream carnfile;
     string name;
     carnfile.open("carnivores.txt");
     while(getline(carnfile, name, '#')) {
         if (animalSearch == name) {
-            isCarn = true;
+            return true;
         }
     }
-    return isCarn;
+    return false;
 }
 
 bool searchHerbivore(string animalSearch) {
-    bool isHerb = false;
     ifstream herbfile;
     string name;
     herbfile.open("herbivores.txt");
     while(getline(herbfile, name, '#')) {
         if (animalSearch == name) {
-            isHerb = true;
+            return true;
         }
     }
-    return isHerb;
+    return false;
 }
 void printDino(ofstream& outfile, ifstream& infile, string animalName, int totalsA[]) {
     string data;
+    size_t find;
     outfile << animalName << endl;
     getline(infile, data, '#');
     outfile << data << endl;
-    if (overTenGrand(animalName)) {
+    getline(infile, data, '#');
+    if(overTenGrand(data)) {
         totalsA[3] += 1;
     }
-    getline(infile, data, '#');
     outfile << data << endl;
     getline(infile, data, '#');
     outfile << data << endl;
@@ -140,8 +146,6 @@ bool overTenGrand(string weightCheck) {
     bool weightCompare;
     int num;
     size_t found;
-    // int num = stoi(mass) should convert
-    //int num = stoi(mass)
 
     found = weightCheck.find("to ");
     if (found != string::npos) {
